de2f224c6b0f6fccbdc47e109479c0e24c06368e
Benedict Osei Sefa <benedictoseisefa@Benedicts-MacBook-Pro.local>
2022-07-14 19:09:34 +0000

    added a scaffold and a floating action button

--- lib/main.dart
+++ lib/main.dart
@@ -20,15 +20,16 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-    return Text('Hello there');
+    return Scaffold(
+      appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(onPressed: null),
+    );
   }
 }
